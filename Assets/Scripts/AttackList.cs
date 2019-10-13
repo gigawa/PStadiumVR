8e2ce69887a7f7f9d32644d9762dc41f67d706be
Grant Hooks <granthooks28@gmail.com>
2019-10-13 16:30:10 -0700

    Revert "Revert "Initial Commit""
    This reverts commit d9e260d143e7cbca9d0ba5d49f718eb571b692c7.

---
+++ Assets/Scripts/AttackList.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PokemonInfo;
+
+public class AttackList
+{
+    public List<AttackInfo> createdAttacks;
+
+    private static AttackList instance = null;
+    private static readonly object padlock = new object();
+
+    public static AttackList Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new AttackList();
+                }
+                return instance;
+            }
+        }
+    }
+
+    AttackList()
+    {
+        createdAttacks = new List<AttackInfo>();
+    }
+
+    public AttackInfo GetAttack(string attackName)
+    {
+        AttackInfo attack = null;
+        bool isCreated = false;
+
+        foreach(AttackInfo created in createdAttacks) {
+            if(attackName == created.name)
+            {
+                attack = created;
+                isCreated = true;
+                break;
+            }
+        }
+
+        if(!isCreated)
+        {
+            attack = CreateAttack(attackName);
+        }
+
+        return attack;
+    }
+
+    private AttackInfo CreateAttack(string attackName)
+    {
+        AttackInfo attack = null;
+        switch(attackName)
+        {
+            case "Tackle":
+                // Physical Attack
+                // Attack Name, Category, Type, PP, Power, Accuracy
+                attack = new AttackInfo("Tackle", Category.physical, PokemonType.normal, 35, 40, 100);
+                break;
+
+            case "Growl":
+                // Status Effect
+                // Attack Name, Type, PP, Accuracy, Stat, Stage
+                attack = new AttackInfo("Growl", PokemonType.normal, 40, 100, Stat.attack, -1);
+                break;
+
+            case "Leech Seed":
+                // Status Condition
+                // Attack Name, Type, PP, Accuracy, Condition
+                attack = new AttackInfo("Leech Seed", PokemonType.grass, 10, 100, StatusEffects.leechSeed);
+                break;
+
+            case "Vine Whip":
+                // Physical Attack
+                // Attack Name, Category, Type, PP, Power, Accuracy
+                attack = new AttackInfo("Vine Whip", Category.physical, PokemonType.grass, 25, 45, 100);
+                break;
+
+            default:
+                attack = new AttackInfo("Unknown", Category.physical, PokemonType.normal, 0, 0, 0);
+                break;
+        }
+
+        createdAttacks.Add(attack);
+        return attack;
+    }
+}
---
+++ Assets/Scripts/PokemonInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PokemonInfo
+{
+    public enum PokemonType { normal, fighting, flying, poison, ground, rock, bug, ghost, steel, fire, water, grass, electric, psychic, ice, dragon, dark };
+    public enum Category { physical, special, status, effect };
+    public enum Stat { none, hp, attack, defense, spAttack, spDefense, speed };
+    public enum StatusEffects { none, paralyzed, poisoned, badlyPoisoned, burned, frozen, flinch, confused, infatuation, leechSeed }
+}
